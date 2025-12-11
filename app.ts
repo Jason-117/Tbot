eea21b0d73ca930b26f83f9c645c741d901b517e
Json-117 <king66666a@gmail.com>
2025-12-11 15:26:45 +0800

    HT

--- app.ts
+++ app.ts
@@ -240,7 +240,7 @@ bot.on("message", async (ctx) => {
     }
 
     // 处理普通用户消息，将消息推送至管理员
-    const userText = `  新消息来自@${username}
+    const userText = `新消息来自@${username}
     ${ctx.message.text || ctx.message.caption || "用户发送消息非文本消息"}
     `;
 
